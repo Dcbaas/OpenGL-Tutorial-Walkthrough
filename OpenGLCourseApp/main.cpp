2830085e412448d0d00e51a9c5e8a1dbab54dd93
Dcbaas <bcdave.301@gmail.com>
2018-11-01 17:33:09 -0400

    Broken triangle

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -7,12 +7,14 @@
 #include <string>
 #include <GL/glew.h>
 #include <GlfW/glfw3.h>
-
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 const GLint WIDTH = 800, HEIGHT = 600;
 
 //For the first triangle we are drawing these are global variales
 //In future find out how not to use global variables
-GLuint VAO, VBO, shader , uniformXMove; 
+GLuint VAO, VBO, shader , uniformModel; 
 
 bool direction = true; 
 float triOffset = 0.0f;
@@ -25,11 +27,11 @@ static const char* vShader = "                                                \n
                                                                               \n\
 layout (location = 0) in vec3 pos;											  \n\
                                                                               \n\
-uniform float xMove;                                                          \n\
+uniform mat4 model;                                                          \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);		  \n\
+    gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);		  \n\
 }";
 
 //Fragment Shader
@@ -127,7 +129,7 @@ void CompileShaders() {
 		std::cout << "Error validating program" << std::endl << eLog << std::endl;
 	}
 
-	uniformXMove = glGetUniformLocation(shader, "xMove");
+	uniformModel = glGetUniformLocation(shader, "model");
 }
 
 int main(int argc, char** argv) {
@@ -204,7 +206,10 @@ int main(int argc, char** argv) {
 
 		glUseProgram(shader);
 
-		glUniform1f(uniformXMove, triOffset);
+		glm::mat4 model;
+		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
 
